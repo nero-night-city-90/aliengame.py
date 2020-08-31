a1c1605838b793850e8d3853238c0c55dec14633
Nero <68517784+nero-night-city-90@users.noreply.github.com>
2020-08-30 20:28:51 -0400

    Game statistics
    Alien cyber invasion game statistics.

---
+++ game_stats.py
@@ -0,0 +1,15 @@
+
+class GameStats:
+    """Track statistics for Alien Invasion."""
+
+    def __init__(self, ai_game):
+        """Initialize statistics."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
+
+        # Start Alien Cyber Invasion in an active state.
+        self.game_active = True
